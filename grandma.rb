b9835f19f8cfea4a3bd7f3ff4da617eacf88073d
Diane Crame <dcrame@icloud.com>
2020-03-23 22:21:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase.downcase || phrase.swapcase
+  if phrase.downcase || phrase.swapcase || phrase.capitalize
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
