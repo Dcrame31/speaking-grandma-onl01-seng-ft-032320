90da844653a364be4f7a62b02e94384e5a80a844
Diane Crame <dcrame@icloud.com>
2020-03-23 22:34:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,6 @@
 def speak_to_grandma(phrase)
   if phrase === "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  if phrase.upcase
-    return "NO, NOT SINCE 1938!"
   else 
     return "HUH?! SPEAK UP, SONNY!"
    end
